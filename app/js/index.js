c5b9d48fa2b120af27f2fdf315a616c83f4d0bfd
RonaldShum <khshumab@connect.ust.hk>
2016-11-03 15:10:41 +0800

    basic login fuction

--- app/js/index.js
+++ app/js/index.js
@@ -28,23 +28,53 @@ $(document).ready(function(){
     	}
     });
 
-	     
-      // Initialize Firebase
-       const config = {
-        apiKey: "AIzaSyCw7pPwTqi5mXO84LEfYOVUOI1_UPTTg94",
-    	authDomain: "teapot-576b6.firebaseapp.com",
-	    databaseURL: "https://teapot-576b6.firebaseio.com",
-        storageBucket: "teapot-576b6.appspot.com",
-        messagingSenderId: "955450264497"
-      };
-        firebase.initializeApp(config);
-        
-		//Add login event
-		$("#btnLogin").click(function(){
-			const email = $('#txtEmail').val();
-			const pass = $('#txtPassword').val();
-		});
-	        
 
+    // Initialize Firebase
+    initalizeFirebase();
+        
+	//Add login event
+	$("#btnLogin").click(function(){
+		//Get email and pass
+		const email = $('#txtEmail').val();
+		const pass = $('#txtPassword').val();
+		const auth = firebase.auth();
+		// Sign in 
+		const promise = auth.signInWithEmailAndPassword(email,pass);
+		promise.catch( e=> console.log(e.message));
+	});
+
+	//Add signup event
+	$("#btnSignUp").click(function(){
+		//Get email and pass
+		// TODO: CHECK 4 REAL EMAILZ	 
+		const email = $('#txtEmail').val();
+		const pass = $('#txtPassword').val();
+		const auth = firebase.auth();
+		// Sign in 
+		const promise = auth.createUserWithEmailAndPassword(email,pass);
+		promise.catch( e=> console.log(e.message));
+		// console.log("i have pressed btnSignUp");
+	});
+
+	//Add Logout event 
+	$("#btnLogout").click(function(){
+		firebase.auth().signOut();
+	});
+
+	        
+	//Add a realtime listener
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+		if(firebaseUser){
+			console.log(firebaseUser);
+			$('#btnLogout').show();
+			$('#login_box').hide();
+		}else{
+			console.log('not logged in');
+			$('#btnLogout').hide();
+			$('#login_box').show();
+			$('#txtEmail').val("");
+			$('#txtPassword').val("");
+		}
+	});
 
 });
